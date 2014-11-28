63026c8493971902920dc902f95fb93197d87046
Dougal Graham <dougalg@gmail.com>
2014-11-28 17:29:39 -0500

    Three JS?

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 (function(){
+
+    this.size = 4;
+
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -7,11 +10,39 @@
     document.body.appendChild( renderer.domElement );
 
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    var material = new THREE.MeshBasicMaterial( {
+        color: 0x00ff00,
+        vertexColors: THREE.VertexColors,
+        depthWrite: false
+    } );
 
-    camera.position.z = 5;
+    var planes = ['x', 'y', 'z'],
+        plane;
+    for (var p=0; p<planes.length; p++) {
+        plane = planes[p];
+        for (var i=0; i < this.size; i++) {
+            var x = [];
+            for (var j=0; j < this.size; j++) {
+                var y = [];
+                for (var k=0; k <= this.size; k++) {
+                    var cube = new THREE.Mesh( geometry, material );
+                    cube.position.x = i;
+                    cube.position.y = j;
+                    cube.position.z = k;
+                    scene.add( cube );
+                }
+                x.push(y);
+            }
+        }
+    }
+
+    camera.position.z = 20;
+
+    function render() {
+        requestAnimationFrame( render );
+        renderer.render( scene, camera );
+    }
+    render();
 
     // var gb = new GameBoard();
     // gb.init();
